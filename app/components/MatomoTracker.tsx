cfd946794189a9c4b6e843ff455621acb58eab7e
krrdash <jpomianowski@outlook.com>
2025-04-07 10:55:50 +0200

    Reverted a commit

--- app/components/MatomoTracker.tsx
+++ app/components/MatomoTracker.tsx
@@ -7,19 +7,16 @@ export const MatomoTracker = () => {
     <>
       <Script id="matomo-config" strategy="afterInteractive">
         {`
-        _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-        _paq.push(["setCookieDomain", "*.jpomian.pl"]);
-        _paq.push(["setDoNotTrack", true]);
-        _paq.push(["disableCookies"]);
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-          var u="//analytics.jpomian.pl/";
-          _paq.push(['setTrackerUrl', u+'matomo.php']);
-          _paq.push(['setSiteId', '4']);
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
+          var _paq = window._paq = window._paq || [];
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//analytics.jpomian.pl/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '3']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
         `}
       </Script>
       <Script
